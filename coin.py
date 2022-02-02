7888e8bc6a65ea4c7805d64f85324a5c5464134a
Luis Stanglmeier <luis.stanglmeier@tsep.com>
2022-02-02 17:08:16 +0100

    added a logging function

--- coin.py
+++ coin.py
@@ -1,4 +1,8 @@
+from genericpath import exists
+from venv import create
+from xmlrpc.client import DateTime
 from pycoingecko import CoinGeckoAPI
+from datetime import datetime
 
 __ALLCOINS = {}
 
@@ -16,16 +20,22 @@ def CoinId(symbol):
 
 class Coin:
 
-  def __init__(self, id, symbol, name):
+  def __init__(self, id: str, symbol: str, name: str, datetime_format="%d.%m.%Y - %H:%M:%S"):
     self.id = id
     self.symbol = symbol
+    self.datetime_format = datetime_format
     print(f"Coin: {name} ({symbol})")
 
-  def GetData(self, currency):
+  def GetData(self, currency: str):
     coin_data = CoinGeckoAPI().get_price(ids=self.id, vs_currencies=currency, include_market_cap=True)
     #print(coin_data)
-    return_string = "Price: " + str(coin_data[self.id][currency]) + '€'
+    self.currentprice = coin_data[self.id][currency]
+    return_string = "Price: " + str(self.currentprice) + '€'
     marketcap = coin_data[self.id]['eur_market_cap']
     if marketcap > 0:
       return_string += " | Market Cap: " + str(marketcap) + '€'
     return return_string
+
+  def SaveData(self):
+    with open("CoinData/" + self.symbol + "Data.coin", "a") as coin_file:
+      coin_file.write(datetime.today().strftime(self.datetime_format) + ": " + str(self.currentprice) + "€\n")
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from time import sleep
 from coin import *
 
 btc = Coin(CoinId("BTC"), "BTC", "Bitcoin")
@@ -10,4 +11,11 @@ dark = Coin(CoinId("DARK"), "DARK", "DarkCrypto")
 print(dark.GetData('eur'))
 
 sky = Coin(CoinId("SKY"), "SKY", "DarkCrypto-Share")
-print(sky.GetData('eur'))
+print(sky.GetData("eur"))
+
+while 1:
+  btc.SaveData()
+  cro.SaveData()
+  dark.SaveData()
+  sky.SaveData()
+  sleep(300)
